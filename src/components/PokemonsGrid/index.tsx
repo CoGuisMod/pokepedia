ed07be81c987d702ae00772c80b337fb066e53b6
CoGuisMod <coguismod@gmail.com>
2023-01-08 15:12:53 -0500

    Little fix.

--- src/components/PokemonsGrid/index.tsx
+++ src/components/PokemonsGrid/index.tsx
@@ -71,7 +71,7 @@ function PokemonsGrid() {
 
       <div className="grid grid-cols-2 md:grid-cols-5 gap-2 w-full">
         {pokemonData
-          ? pokemonData.results.map((pokemon: any, index: number) => (
+          ? pokemonData.results?.map((pokemon: any, index: number) => (
               <PokemonCard key={index} pokemon={pokemon} />
             ))
           : "Loading..."}
